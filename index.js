77a37cd95bfa8e37d871658eac554c63fb0e8bfe
Vjekoslav Majerić <vmajeric33@gmail.com>
2024-12-11 14:44:07 +0100

    Didnt work

--- index.js
+++
@@ -1,4 +0,0 @@
-import  { MapboxExportControl, Size, PageOrientation, Format, DPI} from "./packages/mapbox-gl-export";
-import './packages/mapbox-gl-export/dist/mapbox-gl-export.css';
-
-export  { MapboxExportControl, Size, PageOrientation, Format, DPI};
